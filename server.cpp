d88e8d20ed5191e676f54454ddb24760ac9d3255
l3vith <smitbarve666@gmail.com>
2024-04-25 19:14:11 +0530

    added parsing thru one_request()

--- server.cpp
+++ server.cpp
@@ -9,6 +9,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+const size_t k_max_msg = 4096;
+
 static void die(const char *msg) {
   int err = errno;
   fprintf(stderr, "[%d] %s\n", err, msg);
@@ -31,8 +33,6 @@ static void do_something(int connfd) {
   write(connfd, wbuf, strlen(wbuf));
 }
 
-static void one_request() {}
-
 static int32_t read_full(int fd, char *buf, size_t n) {
   while (n > 0) {
     ssize_t rv = read(fd, buf, n);
@@ -44,6 +44,53 @@ static int32_t read_full(int fd, char *buf, size_t n) {
     buf += rv;
   }
   return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1;
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+static int32_t one_request(int connfd) {
+  // 4 bytes header
+  char rbuf[4 + k_max_msg + 1];
+  errno = 0; // Reseting errno buffer
+  int32_t err = read_full(connfd, rbuf, 4);
+
+  if (err) {
+    if (errno == 0) {
+      msg("EOF");
+    } else {
+      msg("read() error");
+    }
+    return err;
+  }
+
+  // No idea tf happens after this ffs
+
+  uint32_t len = 0;
+  memcpy(&len, rbuf, 4);
+  if (len > k_max_msg) {
+    msg("too long");
+    return -1;
+  }
+
+  err = read_full(connfd, &rbuf[4], len);
+  if (err) {
+    msg("read() error");
+    return err;
+  }
+
+  rbuf[4 + len] = '\0';
+  printf("client says: %s\n", &rbuf[4]);
 }
 
 int main() {
